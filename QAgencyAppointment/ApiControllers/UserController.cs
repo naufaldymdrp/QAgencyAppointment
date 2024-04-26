3d3b3acd86b02b94bb28091411e523129a4f7b6c
naufaldymdrp <naufaldymdrp@outlook.com>
2024-04-27 03:08:20 +0700

    Feature: Refined initial user controller

--- QAgencyAppointment/ApiControllers/UserController.cs
+++ QAgencyAppointment/ApiControllers/UserController.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using QAgencyAppointment.Business.Dtos;
+
+namespace QAgencyAppointment.ApiControllers; 
 
 [ApiController]
-[Route("/api/[controller]")]
+[Route("/api/[controller]s")]
 public class UserController : ControllerBase
 {
-    [HttpGet("/get")]
+    [HttpGet("get")]
     public IActionResult GetUserData()
     {
-        return Ok("Ok");
+        return Ok("Okeh");
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] UserDto userDto)
+    {
+        return Ok(userDto);
     }
 }
