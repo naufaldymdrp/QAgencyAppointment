8989566ef17c9f6b6653f6aac8d79fe6b9104d07
naufaldymdrp <naufaldymdrp@outlook.com>
2024-04-29 05:53:11 +0700

    Feature: Added user repository
    * Notes:
      - Mostly accesses UserManager

--- QAgencyAppointment.DataAccess/Interfaces/IUserRepository.cs
+++ QAgencyAppointment.DataAccess/Interfaces/IUserRepository.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace QAgencyAppointment.DataAccess.Interfaces;
 
 public interface IUserRepository
 {
+    public Task<IdentityUser> FindByUsernameAsync(string username);
 
+    public Task<bool> CheckPasswordAsync(IdentityUser user, string password);
+
+    public Task<IList<string>> GetAllRoleNamesByUserAsync(IdentityUser user);
 }
--- QAgencyAppointment.DataAccess/Repositories/UserRepository.cs
+++ QAgencyAppointment.DataAccess/Repositories/UserRepository.cs
@@ -1,8 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using QAgencyAppointment.DataAccess.Entities;
 using QAgencyAppointment.DataAccess.Interfaces;
 
 namespace QAgencyAppointment.DataAccess.Repositories;
 
 public class UserRepository : IUserRepository
 {
+    private readonly UserManager<IdentityUser> _userManager;
 
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public UserRepository(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IdentityUser?> FindByUsernameAsync(string username)
+        => await _userManager.FindByNameAsync(username);
+
+    public async Task<bool> CheckPasswordAsync(IdentityUser user, string password)
+        => await _userManager.CheckPasswordAsync(user, password);
+
+    public async Task<IList<string>> GetAllRoleNamesByUserAsync(IdentityUser user)
+        => await _userManager.GetRolesAsync(user);
 }
